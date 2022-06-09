f5a2e36ae48d3aa4bd2fedf7f4c3adba440a328e
Emmett Deen <emmett.deen@kickasscoding.com>
2022-06-09 17:04:37 -0400

    Foreground notification popup

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,15 +1,15 @@
 import 'dart:async';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_atlanta_push_notification_example/app/data/models/notification.dart';
+import 'package:flutter_atlanta_push_notification_example/app/data/models/notification.dart' as model;
 import 'package:flutter_atlanta_push_notification_example/app/data/models/user.dart';
 import 'package:flutter_atlanta_push_notification_example/app/data/services/notification_service.dart';
 import 'package:flutter_atlanta_push_notification_example/app/data/services/user_service.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  StreamSubscription<List<Notification>>? _notificationSubscription;
-  final RxList<Notification> notifications = RxList<Notification>();
+  StreamSubscription<List<model.Notification>>? _notificationSubscription;
+  final RxList<model.Notification> notifications = RxList<model.Notification>();
   @override
   void onInit() {
     super.onInit();
@@ -25,17 +25,22 @@ class HomeController extends GetxController {
   }
 
   void _storeToken() async {
+    FirebaseMessaging.onMessage.listen((event) {
+      Get.rawSnackbar(
+          title: event.notification?.title ?? 'Notification',
+          message: event.notification?.body ?? '',
+          snackPosition: SnackPosition.TOP);
+    });
     FirebaseMessaging.instance.requestPermission();
 
-    UserService().create(User(fcmToken: await FirebaseMessaging.instance.getToken()));
-
-    
+    UserService()
+        .create(User(fcmToken: await FirebaseMessaging.instance.getToken()));
   }
 
   void _listenForNotifications() {
     _notificationSubscription =
         NotificationService().streamList(limit: 1000).listen(
-      (List<Notification> notifications) {
+      (List<model.Notification> notifications) {
         this.notifications.value = notifications;
       },
     );
